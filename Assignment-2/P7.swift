5a2a082b610013fca5fc10d6d0f6f51ac7aeb08e
Jaydeep Chauhan <66904825+Jaydeep2121@users.noreply.github.com>
2021-10-08 01:45:25 -0500

    Create P7.swift

---
+++ Assignment-2/P7.swift
@@ -0,0 +1,39 @@
+/*
+
+  *   *   *   *   * 
+    *   *   *   * 
+      *   *   * 
+        *   * 
+          * 
+          
+          * 
+        *   * 
+      *   *   * 
+    *   *   *   * 
+  *   *   *   *   *
+
+*/
+var n=10,k=0,m=0
+for i in 0...n{
+  if(i<(n/2)){
+    	for j in 0...(n/2)-1{
+      	   if(k<=j && m<=i){
+      		print("  * ", terminator:"")
+      	   }else{
+      		print("  ", terminator:"")
+      	   }
+    	}
+   k+=1
+  }else if(i>=(n/2)){
+    	for j in 0...(n/2)-1{
+      	   if(k<=j && m<=i){
+      		print("  * ", terminator:"")
+      	   }else{
+      		print("  ", terminator:"")
+      	   }
+    	}
+   k-=1
+  }
+  m+=1
+  print()
+}
