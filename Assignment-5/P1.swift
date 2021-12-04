6f27e2db3f2218ac41612b00d622273c6a07317e
Jaydeep Chauhan <66904825+Jaydeep2121@users.noreply.github.com>
2021-12-04 10:03:04 -0500

    Update P1.swift

--- Assignment-5/P1.swift
+++ Assignment-5/P1.swift
@@ -1,8 +1,4 @@
 /*
-  You have to submit a text file which will have:
-  1. URL of your GitHub repo
-
-  2. One or more code modules that implement and justify the following:
   - Error Handling (throw custom errors from a function and catch them while calling the function)
   - Guard
   - Functions (use variadic parameters, inout parameters)
